e031009d68d00218bfd0c9eb121ed37266047741
Kaung Khant Kyaw <kaungkhantkyaw@Kaungs-MacBook-Air.local>
2023-09-22 02:31:20 +0630

    kkk

--- lib/src/screens/history_screen.dart
+++ lib/src/screens/history_screen.dart
@@ -1,4 +1,6 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:pos/global.dart';
 import 'package:pos/src/constants/font_constants.dart';
 import 'package:pos/src/screens/bottombar_screen.dart';
@@ -14,6 +16,7 @@ class HistoryScreen extends StatefulWidget {
 
 class _HistoryScreenState extends State<HistoryScreen> {
   final orderService = OrderService();
+  final ScrollController _orderController = ScrollController();
   List orders = [];
 
   @override
@@ -33,7 +36,23 @@ class _HistoryScreenState extends State<HistoryScreen> {
       final response = await orderService.getOrdersData();
       if (response!["code"] == 200) {
         if (response["data"].isNotEmpty) {
-          orders = response["data"];
+          List data = response["data"];
+
+          final groupedItems = groupBy(data, (item) => item["table_number"]);
+          final groupedItemsMap = Map.from(groupedItems);
+
+          groupedItemsMap.forEach((tableNumber, items) {
+            orders.add({
+              "title": tableNumber,
+              "items": items,
+            });
+          });
+
+          orders.sort((a, b) {
+            final int tableNumberA = a["title"];
+            final int tableNumberB = b["title"];
+            return tableNumberA.compareTo(tableNumberB);
+          });
         }
         setState(() {});
       } else {
@@ -42,6 +61,16 @@ class _HistoryScreenState extends State<HistoryScreen> {
     } catch (e) {
       print('Error: $e');
     }
+  }
+
+  String formatNumber(int number) {
+    return number.toString().padLeft(6, '0');
+  }
+
+  String formatTimestamp(String timestamp) {
+    final dateTime = DateTime.parse(timestamp);
+    final formattedDate = DateFormat.yMMMd().add_jm().format(dateTime);
+    return formattedDate;
   }
 
   @override
@@ -54,6 +83,104 @@ class _HistoryScreenState extends State<HistoryScreen> {
         title: Text(
           language["History"] ?? "History",
           style: FontConstants.title1,
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 24,
+          ),
+          width: double.infinity,
+          child: ListView.builder(
+            controller: _orderController,
+            scrollDirection: Axis.vertical,
+            shrinkWrap: true,
+            itemCount: orders.length,
+            itemBuilder: (context, index) {
+              return Container(
+                margin: const EdgeInsets.only(
+                  bottom: 8,
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.white,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 16,
+                        right: 16,
+                        top: 8,
+                        bottom: 4,
+                      ),
+                      child: Text(
+                        'Table ${orders[index]["title"]}',
+                        style: FontConstants.body1,
+                      ),
+                    ),
+                    const Divider(
+                      height: 0,
+                      color: Colors.grey,
+                    ),
+                    ListView.builder(
+                      controller: _orderController,
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      itemCount: orders[index]["items"].length,
+                      itemBuilder: (context, i) {
+                        return Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.only(
+                                left: 16,
+                                right: 16,
+                                bottom: 8,
+                                top: 8,
+                              ),
+                              color: Colors.transparent,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    'ORD-${formatNumber(orders[index]["items"][i]["id"])}',
+                                    style: FontConstants.body1,
+                                  ),
+                                  Text(
+                                    formatTimestamp(orders[index]["items"][i]
+                                            ["created_at"]
+                                        .toString()),
+                                    style: FontConstants.caption1,
+                                  ),
+                                ],
+                              ),
+                            ),
+                            i < orders[index]["items"].length - 1
+                                ? Container(
+                                    padding: const EdgeInsets.only(
+                                      left: 16,
+                                      right: 16,
+                                    ),
+                                    child: const Divider(
+                                      height: 0,
+                                      color: Colors.grey,
+                                    ),
+                                  )
+                                : Container(),
+                          ],
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
         ),
       ),
       bottomNavigationBar: const BottomBarScreen(),
